8add7154c0fd3dcfd5ced8e2fde45823e1a45a98
Jon Stapleton <stapleton.jon.michael@gmail.com>
2021-06-17 16:07:37 -0400

    Initial commit

---
+++ src/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+    let count = 0;
+
+    setInterval(function () {
+        count += 1;
+    }, 1000);
+</script>
+
+<svelte:options tag="counter-app"></svelte:options>
+
+<main>
+    <h1>Count: {count}</h1>
+</main>
